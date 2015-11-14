24816dd5e8a3b89f4c4aece2a80ba2a97940f757
Adam Wills <heavymetaladam@gmail.com>
2015-11-14 15:51:32 -0500

    scaffolding using wppbp

---
+++ admin/partials/hnhu-tick-results-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://adamwills.io
+ * @since      1.0.0
+ *
+ * @package    Hnhu_Tick_Results
+ * @subpackage Hnhu_Tick_Results/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ includes/class-hnhu-tick-results-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://adamwills.io
+ * @since      1.0.0
+ *
+ * @package    Hnhu_Tick_Results
+ * @subpackage Hnhu_Tick_Results/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Hnhu_Tick_Results
+ * @subpackage Hnhu_Tick_Results/includes
+ * @author     Adam Wills <adam@adamwills.com>
+ */
+class Hnhu_Tick_Results_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
